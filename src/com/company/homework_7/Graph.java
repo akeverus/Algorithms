14c76d57e40a7f24f0b7030482a314be49b7d36f
Thanatos <polaroids.of.tarkovsky@gmail.com>
2021-12-29 17:48:16 +0300

    homework_7

---
+++ src/com/company/homework_7/Graph.java
@@ -0,0 +1,24 @@
+package com.company.homework_7;
+
+public interface Graph {
+
+    void addVertex(String label);
+
+    boolean addEdge(String startLabel, String secondLabel, String... others);
+    boolean addEdge(String startLabel, String secondLabel);
+
+    int getSize();
+
+    void display();
+
+    /**
+     * англ. Depth-first search, DFS
+     */
+    void dfs(String startLabel);
+
+    /**
+     * англ. breadth-first search, BFS
+     */
+    void bfs(String startLabel);
+
+}
